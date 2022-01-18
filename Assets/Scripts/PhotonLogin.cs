1aef48cb42afdba57f795f40277777f40404e0ac
Dmitriy Starokozhev <p1cachu@yandex.ru>
2022-01-18 22:21:31 +0300

    Photon and PlayFab SDK installed and configured

---
+++ Assets/Scripts/PhotonLogin.cs
@@ -0,0 +1,33 @@
+using Photon.Pun;
+using UnityEngine;
+
+public class PhotonLogin : MonoBehaviourPunCallbacks
+{
+    private void Awake()
+    {
+        PhotonNetwork.AutomaticallySyncScene = true;
+    }
+
+    private void Start()
+    {
+        Connect();
+    }
+
+    public void Connect()
+    {
+        if (PhotonNetwork.IsConnected)
+        {
+            PhotonNetwork.JoinRandomRoom();
+        }
+        else
+        {
+            PhotonNetwork.ConnectUsingSettings();
+        }
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        base.OnConnectedToMaster();
+        Debug.Log("Photon Success");
+    }
+}
