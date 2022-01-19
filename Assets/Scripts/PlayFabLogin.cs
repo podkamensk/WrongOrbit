a56e20a98876ac3e7f4e9eeb7e7273592d0ebc77
podkamensk <roman.patarushin@yahoo.com>
2022-01-19 18:52:21 +0000

    Rolled Back Photon
    Rolled Back Photon for step-by-step understanding of the matter

--- Assets/Scripts/PlayFabLogin.cs
+++ Assets/Scripts/PlayFabLogin.cs
@@ -2,27 +2,27 @@ using PlayFab;
 using PlayFab.ClientModels;
 using UnityEngine;
 
-public class PlayFabLogin : MonoBehaviour
-{
-    private void Start()
+    public class PlayFabLogin : MonoBehaviour
     {
-        if (string.IsNullOrEmpty(PlayFabSettings.staticSettings.TitleId))
+        private void Start()
         {
-            PlayFabSettings.staticSettings.TitleId = "A823B";
-            Debug.Log("Title ID was installed");
+            if (string.IsNullOrEmpty(PlayFabSettings.staticSettings.TitleId))
+            {
+                PlayFabSettings.staticSettings.TitleId = "D2004";
+                Debug.Log("Title ID was installed");
+            }
+
+            var request = new LoginWithCustomIDRequest { CustomId = "lesson3", CreateAccount = true };
+            PlayFabClientAPI.LoginWithCustomID(request, OnLoginSuccess, OnLoginFailure);
         }
 
-        var request = new LoginWithCustomIDRequest {CustomId = "lesson3", CreateAccount = true};
-        PlayFabClientAPI.LoginWithCustomID(request, OnLoginSuccess, OnLoginFailure);
-    }
+        private void OnLoginSuccess(LoginResult result)
+        {
+            Debug.Log("PlayFab Success");
+        }
 
-    private void OnLoginSuccess(LoginResult result)
-    {
-        Debug.Log("PlayFab Success");
+        private void OnLoginFailure(PlayFabError error)
+        {
+            Debug.LogError($"Fail: {error}");
+        }
     }
-
-    private void OnLoginFailure(PlayFabError error)
-    {
-        Debug.LogError($"Fail: {error}");
-    }
-}
