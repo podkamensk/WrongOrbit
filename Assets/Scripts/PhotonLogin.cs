040a592b2706da64f10daf86bc0687e487d3fdab
podkamensk <roman.patarushin@yahoo.com>
2022-01-24 17:02:01 +0000

    Homework 3 done
    Added requested UI elements

--- Assets/Scripts/PhotonLogin.cs
+++ Assets/Scripts/PhotonLogin.cs
@@ -2,22 +2,43 @@
 using UnityEngine;
 using Photon.Pun;
 using Photon.Realtime;
+using TMPro;
+using UnityEngine.UI;
 
 namespace WrongOrbit 
 {
+
     public class PhotonLogin : MonoBehaviourPunCallbacks
     {
+        [SerializeField] private TMP_Text _statusText;
+        [SerializeField] private Button _button;
+        [SerializeField] private TMP_Text _buttonText;
+
+        private Color _textColorDefault = Color.black;
+        private Color _textColorConnected = Color.green;
+        private Color _textColorDisconnected = Color.red;
+
         string _gameVersion = "1"; //hardcode
         void Awake()
         {
-            PhotonNetwork.AutomaticallySyncScene = true; //later to class init
+            PhotonNetwork.AutomaticallySyncScene = true;
+            _button.onClick.AddListener(TaskOnClick);
+
+            if (PhotonNetwork.IsConnected)
+            {
+                _statusText.text = "Photon: connected";
+                _statusText.color = _textColorConnected;
+                _buttonText.text = "disconnect";
+            }
+            else
+            {
+                _statusText.text = "Photon: disconnected";
+                _statusText.color = _textColorDefault;
+                _buttonText.text = "connect";
+            }
 
         }
-        // Start is called before the first frame update
-        void Start()
-        {
-            Connect();
-        }
+
 
         public void Connect()
         {
@@ -40,14 +61,31 @@ namespace WrongOrbit
         public override void OnConnectedToMaster()
         {
             Debug.Log("OnConnectedToMaster() was called by PUN");
+            _statusText.text = "Photon: connected";
+            _statusText.color = _textColorConnected;
+            _buttonText.text = "disconnect";
         }
 
         public override void OnDisconnected(DisconnectCause cause)
         {
             Debug.Log("PHOTON Disconnected: " + cause.ToString());
+            _statusText.text = "Photon: disconnected";
+            _statusText.color = _textColorDisconnected;
+            _buttonText.text = "connect";
         }
 
-
+        public void TaskOnClick()
+        {
+            if (PhotonNetwork.IsConnected)
+            {
+                Disconnect();
+            }
+            else
+            {
+                Connect();
+            }
+            _buttonText.text = "wait";
+        }
     }
 }
 
--- Assets/Scripts/PlayFabLogin.cs
+++ Assets/Scripts/PlayFabLogin.cs
@@ -1,12 +1,52 @@
 using PlayFab;
 using PlayFab.ClientModels;
 using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
 
 namespace WrongOrbit
 {
+    
     public class PlayFabLogin : MonoBehaviour
     {
-        private void Start()
+        [SerializeField] private TMP_Text _statusText;
+        [SerializeField] public Button _button;
+        [SerializeField] private TMP_Text _buttonText;
+        
+
+        private ColorBlock _colors;
+        private Color _textColorDefault = Color.black;
+        private Color _textColorConnected = Color.green;
+
+        private void Awake()
+        {
+            _button.onClick.AddListener(TaskOnClick);
+
+            _statusText.text = "Playfab: disconnected";
+            _buttonText.text = "connect";
+
+            _statusText.color = _textColorDefault;
+
+        }
+
+        private void OnLoginSuccess(LoginResult result)
+        {
+            _statusText.text = "Playfab: connected";
+            _buttonText.text = "refresh";
+            _statusText.color = _textColorConnected;
+            Debug.Log("PlayFab Login Success");
+
+        }
+
+        private void OnLoginFailure(PlayFabError error)
+        {
+            _statusText.text = "Playfab: disconnected";
+            _buttonText.text = "refresh";
+            _statusText.color = _textColorDefault;
+            Debug.LogError($"Fail: {error}");
+        }
+
+        public void TaskOnClick()
         {
             if (string.IsNullOrEmpty(PlayFabSettings.staticSettings.TitleId))
             {
@@ -16,16 +56,9 @@ namespace WrongOrbit
 
             var request = new LoginWithCustomIDRequest { CustomId = "RomanTestCustomID", CreateAccount = true };
             PlayFabClientAPI.LoginWithCustomID(request, OnLoginSuccess, OnLoginFailure);
-        }
-
-        private void OnLoginSuccess(LoginResult result)
-        {
-            Debug.Log("PlayFab Login Success");
-        }
-
-        private void OnLoginFailure(PlayFabError error)
-        {
-            Debug.LogError($"Fail: {error}");
+            _statusText.text = "Playfab: connecting...";
+            _buttonText.text = "wait";
+            _statusText.color = _textColorDefault;
         }
     }
 }
